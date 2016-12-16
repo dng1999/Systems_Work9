b5fa796ca9501803df8d5136df07969ca44b2285
yureistar <dng1@stuy.edu>
2016-12-16 11:50:32 -0500

    sad

--- pipe_networking.c
+++ pipe_networking.c
@@ -10,33 +10,44 @@
 #define MESSAGE_BUFFER_SIZE 100
 
 int client_handshake(int *server){
-  int success = mkfifo("public_pipe",0644);
-  if (success){
-    printf("Error %d: %s\n", errno, strerr(errno));
-    return -1;
-  }
+  mkfifo("public_pipe",0644);
+  printf("[SERVER]: public_pipe made");
+  
   char buffer[MESSAGE_BUFFER_SIZE];
   int pd = open("public_pipe", O_RDONLY);
+  printf("[SERVER]: waiting for client");
   read(pd, buffer, sizeof(buffer));
+  printf("[SERVER]: got message: %s",buffer);
+
   close(pd);
+  system("rm public_pipe");
+  printf("[SERVER]: public_pipe removed");
+
   int pd2 = open(buffer, O_WRONLY);
   char msg = "i gotchu";
   write(pd2, msg, sizeof(msg));
+  printf("[SERVER]: sent message");
+  return pd2;
 }
 
 int server_handshake(int *client){
-  int success = mkfifo("private_pipe",0644);
-  if (success){
-    printf("Error %d: %s\n", errno, strerr(errno));
-    return -1;
-  }
+  char pn = (char *)getpid();
+  mkfifo(pn,0644);
+  printf("[CLIENT]: private pipe made");
+  
   int pd = open("public_pipe", O_WRONLY);
-  char pn = "private_pipe";
   write(pd, pn, sizeof(pn));
-  int pd2 = open("private_pipe", O_RDONLY);
+  int pd2 = open(pn, O_RDONLY);
+  printf("[CLIENT]: sent message");
+
   char buffer[MESSAGE_BUFFER_SIZE];
   read(pd2, buffer, sizeof(buffer));
+  printf("[CLIENT]: got message: %s",buffer);
   close(pd2);
+  printf("[CLIENT]: removed pipe");
+  
+  
   char msg = "same";
   write(pd2, msg, sizeof(msg));
+  return pd2;
 }
